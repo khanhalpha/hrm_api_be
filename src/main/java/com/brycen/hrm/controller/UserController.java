eb4037c9d26555364b114bd6b98e77d03d93815d
DaoKhanh <huukhanh2121@gmail.com>
2020-06-12 07:54:25 +0700

    update 11/06/2020

--- src/main/java/com/brycen/hrm/controller/UserController.java
+++ src/main/java/com/brycen/hrm/controller/UserController.java
@@ -2,15 +2,18 @@ package com.brycen.hrm.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brycen.hrm.model.Skill;
 import com.brycen.hrm.model.User;
 import com.brycen.hrm.repository.UserRepository;
 
@@ -35,6 +38,17 @@ public class UserController {
 	      return new ResponseEntity<>(users, HttpStatus.OK);
 	    } catch (Exception e) {
 	      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	  }
+	
+	 @GetMapping("/users/{id}")
+	  public ResponseEntity<User> getSkill(@PathVariable("id") Long id) {
+	    Optional<User> userData = userRepository.findById(id);
+
+	    if (userData.isPresent()) {
+	      return new ResponseEntity<>(userData.get(), HttpStatus.OK);
+	    } else {
+	      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	    }
 	  }
 }
